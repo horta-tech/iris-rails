1ef766c65d3b886963913e01ae7d25329eac738f
Steven Chern <stevenchern@gmail.com>
2020-04-07 12:37:49 -0300

    make keyphrase in text in strong and delete white space for author

--- lib/generators/templates/app/controllers/iris_rails/posts_controller.rb
+++ lib/generators/templates/app/controllers/iris_rails/posts_controller.rb
@@ -18,13 +18,19 @@ class IrisRails::PostsController < IrisRails::BaseController
     response = IrisRails::Post.find(params[:slug])
     @post, @related_posts, @account = response
     @author = author_name_or_email(@post.author)
+    strongkeyphrase = '<strong>' + @post.keyphrase + '</strong>'
+    @post.first_introduction = @post.first_introduction.gsub(@post.keyphrase, strongkeyphrase)
+    @post.second_introduction = @post.second_introduction.gsub(@post.keyphrase, strongkeyphrase)
+    @post.ordered_topics.each do |topic|
+      topic['content'] = topic['content'].gsub(@post.keyphrase, strongkeyphrase)
+    end
   end
 
   private
 
   def author_name_or_email(author)
     if author['first_name'] || author['last_name']
-      "#{ author['first_name'] } #{ author['last_name'] }"
+      "#{author['first_name'].strip} #{author['last_name'].strip}"
     else
       author['email']
     end
